f6efbb8d949a286d28baa9445e8769d7671cdab8
bpantha <jaypantha21@gmail.com>
2023-06-26 14:00:16 -0700

    head shot update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,7 +6,7 @@ import DownloadIcon from "@mui/icons-material/Download";
 import Resume from "../resume/Bidur_Pantha_Resume.pdf";
 
 import "../styles/Home.css";
-import headshot from "../images/Screenshot 2023-06-26 at 11.23.05 AM.png";
+import headshot from "../images/Screenshot 2023-06-26 at 1.59.07 PM.png";
 
 function Home() {
   return (
