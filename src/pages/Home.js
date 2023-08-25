990ecf1fbdc851a96900ad25d93394b9231a97b0
bpantha <jaypantha21@gmail.com>
2023-08-25 12:31:08 -0700

    home page update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,6 @@ import React from "react";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import EmailIcon from "@mui/icons-material/Email";
-import DownloadIcon from "@mui/icons-material/Download";
 import "../styles/Home.css";
 import headshot from "../images/Screenshot 2023-06-26 at 1.59.07 PM.png";
 import snowy from "../images/snowy_flight.jpg";
@@ -26,9 +25,12 @@ function Home() {
         <div className="headShot">
           <img className="responsive-image" src={headshot} alt="" />
         </div>
+        <div>
+          {" "}
+          <p style={{ fontSize: "30px" }}> Software Engineer </p>
+        </div>
 
         <div className="prompt">
-          <p></p>
           <a
             href="https://www.linkedin.com/in/bidurpantha/"
             target="_blank"
@@ -55,9 +57,6 @@ function Home() {
         <div className="resume">
           <a href={Resume} target="_blank" rel="noopener noreferrer">
             <p style={{ color: "white" }}>Resume</p>
-            <DownloadIcon
-              style={{ fontSize: "70px", color: "white" }}
-            ></DownloadIcon>
           </a>
         </div>
       </div>
