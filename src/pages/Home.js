0bbffdcd7ecc092abc7c1d95600c0a4641c14470
bpantha <jaypantha21@gmail.com>
2025-03-01 14:10:47 -0700

    resume update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -56,7 +56,7 @@ function Home() {
 
         <div className="resume">
           <a href={Resume} target="_blank" rel="noopener noreferrer">
-            <p style={{ color: "white" }}>Resume April 2024</p>
+            <p style={{ color: "white" }}>Resume March 2025</p>
           </a>
         </div>
       </div>
